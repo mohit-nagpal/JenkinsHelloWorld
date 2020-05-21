d20ce2c515b942f10f1772ba908a80c53fcd43cf
mohit-nagpal <64080094+mohit-nagpal@users.noreply.github.com>
2020-05-21 15:35:38 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 public static void main(String[] args){
-System.out.println("HelloWorld...Welcome to Devops world!!!!!!");
+System.out.println("HelloWorld...New changes made!!!!!!");
 }
 }
